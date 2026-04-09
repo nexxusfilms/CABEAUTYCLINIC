a10488fe79d7f3dcadb7d35562e083cb8e96d28b
nexxusfilms <gustavozimmermann44@gmail.com>
2026-04-09 15:34:03 +0000

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,20 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/CABEAUTYCLINIC/', 
+    base: '/', 
+
     plugins: [react(), tailwindcss()],
+
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
+
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
     },
